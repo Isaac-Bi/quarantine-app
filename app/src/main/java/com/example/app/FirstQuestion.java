eabdced0d6b3fa613adfa692a232b1a7f212dc91
camille-004 <dunningcamille@gmail.com>
2020-11-10 20:39:14 -0800

    Second question done

--- app/src/main/java/com/example/app/FirstQuestion.java
+++ app/src/main/java/com/example/app/FirstQuestion.java
@@ -24,9 +24,6 @@ public class FirstQuestion extends AppCompatActivity {
         return questionOneChoice;
     }
 
-
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
