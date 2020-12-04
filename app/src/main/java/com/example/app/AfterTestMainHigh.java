ed605821514fcfeb221e37232c80a9803d785b39
Brian Lu <brianlu2020@gmail.com>
2020-12-03 21:00:34 -0800

    after question pages

--- app/src/main/java/com/example/app/AfterTestMainHigh.java
+++ app/src/main/java/com/example/app/AfterTestMainHigh.java
@@ -2,13 +2,31 @@ package com.example.app;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class AfterTestMainHigh extends AppCompatActivity {
-
+    private TextView highText;
+    private Button homeBtnHigh;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_after_test_main_high);
+
+        highText = (TextView) findViewById(R.id.extremeText);
+        highText.setText(UserData.getInstance().getScore());
+
+        //Next question button
+        homeBtnHigh = (Button)findViewById(R.id.homeBtnHigh);
+        //next question button
+        homeBtnHigh.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(getApplicationContext(),MainActivity.class));
+            }
+        });
     }
 }
